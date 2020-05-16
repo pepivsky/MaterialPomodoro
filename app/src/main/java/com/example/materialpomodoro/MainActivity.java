f74453dd79c78bf79e0be70dcadce4ae0dd605b9
pepivsky <ironglas68@gmail.com>
2020-05-16 13:45:20 -0500

    Agregado Toolbar y código necesario en la clase Main

--- app/src/main/java/com/example/materialpomodoro/MainActivity.java
+++ app/src/main/java/com/example/materialpomodoro/MainActivity.java
@@ -12,6 +12,7 @@ import android.os.Build;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -39,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         mTextViewCountDown = findViewById(R.id.text_view_countdown);
 
